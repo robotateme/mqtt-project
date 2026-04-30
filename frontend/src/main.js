ea4bb5da114ae1a37eb86a4a2f48bbbb8a086057
robotateme <jarwork884@gmail.com>
2026-04-30 11:00:00 +0300

    Откатил сломанный фон и разнес CSS-темы

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import './styles/themes/empire-night.css';
+import './styles/themes/republic-day.css';
+import './styles/themes/tron-neon-night.css';
+import './styles/themes/cellular-automata-day.css';
+import './styles/themes/cellular-automata-night.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
